81882eb5707a379da5036af528b527920062037c
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-09-03 22:34:48 +0300

    add sameSite none and secure true users controller

--- controllers/users.js
+++ controllers/users.js
@@ -55,6 +55,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
+        sameSite: 'none',
+        secure: true,
       });
       res
         .status(200)
